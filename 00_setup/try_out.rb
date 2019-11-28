06da2675debbeeb18fd2583494b9a9678efc6fa3
ryumori <morikawa926ryu@gmail.com>
2019-11-28 15:58:24 +0900

    Create TryOut class

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -5,4 +5,32 @@ class TryOut
   # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
   # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
   # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+
+  attr_accessor :first_name, :middle_name, :last_name
+
+  def initialize(first_name, middle_name=nil, last_name)
+    @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
+  end
+
+  def full_name
+    if middle_name == nil
+      "#{first_name} #{last_name}"
+    else
+      "#{first_name} #{middle_name} #{last_name}"
+    end
+  end
+
+  def upcase_full_name
+    full_name.upcase
+  end
+
+  def upcase_full_name!
+    first_name.upcase!
+    middle_name.upcase! unless middle_name.nil?
+    last_name.upcase!
+
+    full_name
+  end
 end
