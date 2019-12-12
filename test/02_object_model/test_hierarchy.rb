5b155c537d42352b4f1559d5514ebde758832e20
meganemura <meganemura@users.noreply.github.com>
2019-12-12 11:29:13 +0900

    Test whether value= and value are called or not

--- test/02_object_model/test_hierarchy.rb
+++ test/02_object_model/test_hierarchy.rb
@@ -32,9 +32,22 @@ class TestHierarchy < MiniTest::Test
 
   def test_c4_increment
     c4 = C4.new
+    c4.singleton_class.class_eval do
+      def value=(x)
+        @called_setter = true
+        @value = x
+      end
+
+      def value
+        @called_getter = true
+        @value
+      end
+    end
     assert_equal "1", c4.increment
     assert_equal "2", c4.increment
     assert_equal "3", c4.increment
+    assert c4.instance_variable_get(:"@called_setter")
+    assert c4.instance_variable_get(:"@called_getter")
   end
 
   def test_c4_value_methods
