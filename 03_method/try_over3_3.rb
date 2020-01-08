64bc08702cbcb3c20dde00a776a0fac7c3eac9f3
h1kita <shunhikita@gmail.com>
2020-01-08 21:19:58 +0900

    Fix method redefined warning

--- 03_method/try_over3_3.rb
+++ 03_method/try_over3_3.rb
@@ -13,7 +13,7 @@ TryOver3 = Module.new
 class TryOver3::A2
   def initialize(name, value)
     instance_variable_set("@#{name}", value)
-    self.class.attr_reader name.to_sym
+    self.class.attr_reader name.to_sym unless respond_to? name.to_sym
   end
 end
 
