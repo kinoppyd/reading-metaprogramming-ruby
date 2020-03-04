ea13c67d0d743afd2a4baf6c3eef77b30cb6433f
mpg-shota-yamashita <shota.yamashita@medpeer.co.jp>
2020-03-04 15:55:57 +0900

    resolve test_try_out.rb

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -1,8 +1,34 @@
+# このクラスの仕様
+# コンストラクタは、2つまたは3つの引数を受け付ける。引数はそれぞれ、ファーストネーム、ミドルネーム、ラストネームの順で、ミドルネームは省略が可能
+# full_nameメソッドを持つ。これは、ファーストネーム、ミドルネーム、ラストネームを半角スペース1つで結合した文字列を返す。ただし、ミドルネームが省略されている場合に、ファーストネームとラストネームの間には1つのスペースしか置かない
+# first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
+# upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
+# upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+
 class TryOut
-  # このクラスの仕様
-  # コンストラクタは、2つまたは3つの引数を受け付ける。引数はそれぞれ、ファーストネーム、ミドルネーム、ラストネームの順で、ミドルネームは省略が可能。
-  # full_nameメソッドを持つ。これは、ファーストネーム、ミドルネーム、ラストネームを半角スペース1つで結合した文字列を返す。ただし、ミドルネームが省略されている場合に、ファーストネームとラストネームの間には1つのスペースしか置かない
-  # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
-  # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
-  # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+  attr_reader :first_name, :middle_name, :last_name
+  attr_writer :first_name
+
+  def initialize(first_name, middle_name='', last_name)
+    @first_name = first_name
+    @middle_name = middle_name || ''
+    @last_name = last_name
+  end
+
+  def full_name
+    middle_name == '' ? "#{first_name} #{last_name}" : "#{first_name} #{middle_name} #{last_name}"
+  end
+
+  def upcase_full_name
+    full_name.upcase
+  end
+
+  def upcase_full_name!
+    if middle_name == ''
+      @first_name, @last_name = full_name.upcase.split
+    else
+      @first_name, @middle_name, @last_name = full_name.upcase.split
+    end
+    full_name.upcase
+  end
 end
