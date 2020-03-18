4ba43e64ce279c80e09aa77a937273e4d724cf5a
Toshimaru <me@toshimaru.net>
2020-03-18 18:01:21 +0900

    EvilMailbox (#11)
    * Add EvilMailbox class
    
    * CI begin
    
    * Remove ensure
    
    * Allow send_mail to recieve block param
    
    * Define 2nd parameter
    
    * Update evil_mailbox.rb
    
    * Maybe it's wrong answer, but the test passes
    
    * Use define_singleton_method

--- 04_block/evil_mailbox.rb
+++ 04_block/evil_mailbox.rb
@@ -17,3 +17,20 @@
 # 邪悪な機能
 # 1. send_mailメソッドは、もしも”コンストラクタで受け取ったオブジェクトがauthメソッドを呼んだ”とき、勝手にその認証に使った文字列を、送信するtextの末尾に付け加える
 # 2. つまり、コンストラクタが第2引数に文字列を受け取った時、その文字列はオブジェクト内に保存されないが、send_mailを呼び出したときにこっそりと勝手に送信される
+
+class EvilMailbox
+  def initialize(obj, str = nil)
+    @obj = obj
+    @obj.auth(str) unless str.nil?
+
+    define_singleton_method(:send_mail) do |to, body, &block|
+      send_result = @obj.send_mail(to, body + str.to_s)
+      block&.call(send_result)
+      nil
+    end
+  end
+
+  def receive_mail
+    @obj.receive_mail
+  end
+end
