4af80c42ba83fce9bd2e9264db933df072550c75
Shu OGAWARA <expajp@gmail.com>
2020-08-23 19:46:23 +0900

    余分なスペースのdiffを削除

--- test/03_method/test_define.rb
+++ test/03_method/test_define.rb
@@ -22,7 +22,7 @@ class TestDefine < MiniTest::Test
 
   def test_answer_a2
     instance = A2.new(["hoge", "fuga"])
-    
+
     assert_equal true, instance.methods.include?(:dev_team)
     assert_equal "SmartHR Dev Team", instance.hoge_hoge(nil)
     assert_equal "hoge_hogehoge_hoge", instance.hoge_hoge(2)
@@ -32,10 +32,10 @@ class TestDefine < MiniTest::Test
     assert_equal false, another_instance.methods.include?(:hoge_hoge)
     assert_equal false, another_instance.methods.include?(:hoge_fuga)
   end
-  
+
   def test_answer_a2_number
     instance = A2.new([1, 2])
-    
+
     assert_equal true, instance.methods.include?(:dev_team)
     assert_equal "SmartHR Dev Team", instance.hoge_1(nil)
     assert_equal "hoge_1hoge_1", instance.hoge_1(2)
@@ -45,17 +45,17 @@ class TestDefine < MiniTest::Test
     assert_equal false, another_instance.methods.include?(:hoge_1)
     assert_equal false, another_instance.methods.include?(:hoge_2)
   end
-  
+
   def test_answer_a2_random_name
     value_one = SecureRandom.hex
     value_two = SecureRandom.hex
-    
+
     instance = A2.new([value_one, value_two])
     assert_equal true, instance.methods.include?(:dev_team)
     assert_equal "SmartHR Dev Team", instance.send("hoge_#{value_one}".to_sym, nil)
     assert_equal "hoge_#{value_one}hoge_#{value_one}", instance.send("hoge_#{value_one}".to_sym, 2)
     assert_equal "hoge_#{value_two}hoge_#{value_two}hoge_#{value_two}", instance.send("hoge_#{value_two}".to_sym, 3)
-    
+
     another_instance = A2.new([])
     assert_equal false, another_instance.methods.include?("hoge_#{value_one}".to_sym)
     assert_equal false, another_instance.methods.include?("hoge_#{value_two}".to_sym)
