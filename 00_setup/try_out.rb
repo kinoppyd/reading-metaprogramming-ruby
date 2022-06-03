eb732509cb7d1b1a87bb69de09adf3af41afe642
canisterism <ohtthstc@gmail.com>
2022-06-03 19:45:17 +0900

    solved

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -1,4 +1,35 @@
 class TryOut
+
+  def initialize(*names)
+    raise ArgumentError if names.length <= 1 || names.length > 3
+
+    middle_name = names[1] if names.length == 3
+
+    @first_name = names.first
+    @middle_name = middle_name
+    @last_name = names.last
+  end
+
+  def full_name
+    middle = "#{@middle_name} " if @middle_name
+    "#{@first_name} #{middle}#{@last_name}"
+  end
+
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  def upcase_full_name
+    full_name.upcase
+  end
+
+  def upcase_full_name!
+    @first_name.upcase!
+    @middle_name&.upcase!
+    @last_name.upcase!
+
+    full_name
+  end
   # このクラスの仕様
   # コンストラクタは、2つまたは3つの引数を受け付ける。引数はそれぞれ、ファーストネーム、ミドルネーム、ラストネームの順で、ミドルネームは省略が可能。
   # full_nameメソッドを持つ。これは、ファーストネーム、ミドルネーム、ラストネームを半角スペース1つで結合した文字列を返す。ただし、ミドルネームが省略されている場合に、ファーストネームとラストネームの間には1つのスペースしか置かない
