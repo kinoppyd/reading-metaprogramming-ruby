a750810e4d9159e325dba8b6ca9d63dfd92724c0
canisterism <ohtthstc@gmail.com>
2022-06-03 19:51:34 +0900

    refactored

--- 00_setup/try_out.rb
+++ 00_setup/try_out.rb
@@ -1,22 +1,20 @@
+# このクラスの仕様
+# コンストラクタは、2つまたは3つの引数を受け付ける。引数はそれぞれ、ファーストネーム、ミドルネーム、ラストネームの順で、ミドルネームは省略が可能。
+# full_nameメソッドを持つ。これは、ファーストネーム、ミドルネーム、ラストネームを半角スペース1つで結合した文字列を返す。ただし、ミドルネームが省略されている場合に、ファーストネームとラストネームの間には1つのスペースしか置かない
+# first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
+# upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
+# upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
 class TryOut
+  attr_accessor :first_name
 
-  def initialize(*names)
-    raise ArgumentError if names.length <= 1 || names.length > 3
-
-    middle_name = names[1] if names.length == 3
-
-    @first_name = names.first
+  def initialize(first_name, middle_name = '', last_name)
+    @first_name = first_name
     @middle_name = middle_name
-    @last_name = names.last
+    @last_name = last_name
   end
 
   def full_name
-    middle = "#{@middle_name} " if @middle_name
-    "#{@first_name} #{middle}#{@last_name}"
-  end
-
-  def first_name=(first_name)
-    @first_name = first_name
+    [first_name, middle_name, last_name].reject(&:empty?).join(" ")
   end
 
   def upcase_full_name
@@ -24,16 +22,18 @@ class TryOut
   end
 
   def upcase_full_name!
-    @first_name.upcase!
-    @middle_name&.upcase!
-    @last_name.upcase!
+    upcase_names!
 
     full_name
   end
-  # このクラスの仕様
-  # コンストラクタは、2つまたは3つの引数を受け付ける。引数はそれぞれ、ファーストネーム、ミドルネーム、ラストネームの順で、ミドルネームは省略が可能。
-  # full_nameメソッドを持つ。これは、ファーストネーム、ミドルネーム、ラストネームを半角スペース1つで結合した文字列を返す。ただし、ミドルネームが省略されている場合に、ファーストネームとラストネームの間には1つのスペースしか置かない
-  # first_name=メソッドを持つ。これは、引数の内容でファーストネームを書き換える。
-  # upcase_full_nameメソッドを持つ。これは、full_nameメソッドの結果をすべて大文字で返す。このメソッドは副作用を持たない。
-  # upcase_full_name! メソッドを持つ。これは、upcase_full_nameの副作用を持つバージョンで、ファーストネーム、ミドルネーム、ラストネームをすべて大文字に変え、オブジェクトはその状態を記憶する
+
+  private
+
+  attr_accessor :middle_name, :last_name
+
+  def upcase_names!
+    self.first_name = first_name.upcase
+    self.middle_name = middle_name.upcase
+    self.last_name = last_name.upcase
+  end
 end
