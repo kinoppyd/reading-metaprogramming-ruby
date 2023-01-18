1f227a5c16eaa2ab3441eca91833f7881a1a690b
mpg-masaki-nagasawa <masaki.nagasawa@medpeer.co.jp>
2023-01-18 16:00:26 +0900

    2章のオープンクラスの解答

--- 02_object_model/01_hoge.rb
+++ 02_object_model/01_hoge.rb
@@ -1,19 +1,57 @@
 # Q1.
 # Hogeクラスは次の仕様を持つ
-# "hoge" という文字列の定数Hogeを持つ
-# "hoge" という文字列を返すhogehogeメソッドを持つ
 # HogeクラスのスーパークラスはStringである
-# 自身が"hoge"という文字列である時（HogeクラスはStringがスーパークラスなので、当然自身は文字列である）、trueを返すhoge?メソッドが定義されている
+class Hoge < String
+  # "hoge" という文字列の定数Hogeを持つ
+  Hoge = "hoge"
 
-class Hoge
+  # "hoge" という文字列を返すhogehogeメソッドを持つ
+  def hogehoge
+    "hoge"
+  end
+
+  # 自身が"hoge"という文字列である時（HogeクラスはStringがスーパークラスなので、当然自身は文字列である）、trueを返すhoge?メソッドが定義されている
+  def hoge?
+    self == "hoge"
+  end
 end
 
 # Q2.
 # 次に挙げるクラスのいかなるインスタンスからも、hogeメソッドが呼び出せるようにする
 # それらのhogeメソッドは、全て"hoge"という文字列を返す
 # - String
+class String
+  def hoge
+    "hoge"
+  end
+end
 # - Integer
+class Integer
+  def hoge
+    "hoge"
+  end
+end
 # - Numeric
+class Numeric
+  def hoge
+    "hoge"
+  end
+end
 # - Class
+class Class
+  def hoge
+    "hoge"
+  end
+end
 # - Hash
+class Hash
+  def hoge
+    "hoge"
+  end
+end
 # - TrueClass
+class TrueClass
+  def hoge
+    "hoge"
+  end
+end
